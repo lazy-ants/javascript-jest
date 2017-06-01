05e1e8b5f64d38e4617b1d56a7b6865800be69ef
athlonUA <alexgarmatenko@gmail.com>
2017-06-01 19:44:47 +0300

    Remove unused code

--- src/User.js
+++ src/User.js
@@ -5,16 +5,4 @@ function User(name, age, email, additionalData) {
     this.additionalData = additionalData;
 }
 
-User.prototype.getName = function getName() {
-    return this.name;
-};
-
-User.prototype.getAge = function getAge() {
-    return this.age;
-};
-
-User.prototype.getAdditionalData = function getAdditionalData() {
-    return this.additionalData;
-};
-
 module.exports = User;
